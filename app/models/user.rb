cd692529fba17942da48d9996da8e89541c5bfca
Ebinesh <ebinesh2511@gmail.com>
2024-02-23 00:20:05 +0530

    Role based profiles is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   has_one :farmer_profile
   has_one :business_owner_profile
 
+  def profile
+    return farmer_profile if role == 0
+    return business_owner_profile if role == 1
+  end
 
   def email_required?
     false
